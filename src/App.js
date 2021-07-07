68b1e327c55a94f0b65826434b180f5557f71168
AbdulazizSulaymon <djshahrick@gmail.com>
2021-07-07 20:55:51 +0500

    title ozgardi

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          React bo'yicha 1 - loyiha G92 guruh bilan
         </p>
         <a
           className="App-link"
